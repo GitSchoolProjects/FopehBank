e190d6c314f199c81ca975a384e96b088018acee
Hippolyte <github.09.07.2019@gmail.com>
2019-07-09 15:08:08 +0000

    Le Test

---
+++ Site/Frontend/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./Outils/Semantic/semantic.min.css">
+    <title>Test</title>
+</head>
+<body>
+
+    
+    <!-- Boutton de test pour le lancement des interfaces -->
+    <button class="ui blue button" id="lancerInterface">Cliquez ici</button>
+
+
+    <script src="./Annexe/Js/jquery.js"></script>
+    <script src="./Outils/Semantic/semantic.min.js"></script>
+    <script src="./Annexe/Js/index.js"></script>
+</body>
+</html>
