5e65c8db243e640ef19b01fd2a58da22a8fba9aa
Hippolyte <github.09.07.2019@gmail.com>
2019-07-09 20:45:29 +0000

    Ecriture de Charte.php v1

---
+++ Site/Frontend/Accueil/Charte.php
@@ -0,0 +1,28 @@
+<!-- Code -->
+
+<div class="ui modal" id="modalDeLaCharte">
+    <!-- L'entête -->
+    <div class="header">
+        <i class="file alternate outline icon"></i>
+        Charte N° "bdd"
+    </div>
+    <!-- Le contenu -->
+    <div class="scrolling content">
+        <p>"bdd"</p>
+    </div>
+    <!-- Lest bouttons -->
+    <div class="actions">
+        <div class="ui animated red button" id="fermerModalDeLaCharte">
+            <div class="visible content">Fermer</div>
+            <div class="hidden content">
+                <i class="close icon"></i>
+            </div>
+        </div>
+        <div class="ui vertical animated green button" id="accepterCharte">
+            <div class="visible content">Choisir et accepter</div>
+            <div class="hidden content">
+                <i class="thumbs up outline icon"></i>
+            </div>
+        </div>
+    </div>
+</div>
